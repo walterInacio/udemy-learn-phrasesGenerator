015479d6b36b990e583ed91bc1658963761ad2e8
Walter <walter.inacio28@gmail.com>
2022-08-26 22:57:27 -0300

    first commit

--- dayPhrases/ViewController.swift
+++ dayPhrases/ViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lblMessage: UILabel!
+    
+    @IBAction func generateNewPhrase(_ sender: Any) {
+        var phrases: [String] = []
+        
+        phrases.append("Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos")
+        phrases.append("Ter sucesso é falhar repetidamente, mas sem perder entusiasmo")
+        phrases.append("Não é mais forte que sobrevive, nem o mais inteligente. Quem sobrevive é o mais disposto à mudança")
 
+        lblMessage.text = phrases[Int(arc4random_uniform(3))];
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
